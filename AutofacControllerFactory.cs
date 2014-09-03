5d2c7e46dd96b51efd3e2fb4038a892493c0ac53
hongduo168 <hongduo168@126.com>
2014-09-03 18:14:57 +0800

    新增文件

---
+++ AutofacControllerFactory.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Web.Mvc;
+
+namespace Name
+{
+    public class AutofacControllerFactory : DefaultControllerFactory
+    {
+        private IContainer _container;
+        public AutofacControllerFactory(IContainer container)
+        {
+            _container = container;
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return _container.ResolveKeyed<IController>(controllerType.Name);
+        }
+
+    }
+}
---
+++ DownloadFile.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Web;
+
+namespace DownloadFile
+{
+    public class DownloadFile
+    {
+        #region ResponseFile 输出硬盘文件，提供下载 支持大文件、续传、速度限制、资源占用小
+        /// <summary>
+        ///  输出硬盘文件，提供下载 支持大文件、续传、速度限制、资源占用小
+        /// </summary>
+        /// <param name="request">Page.Request对象</param>
+        /// <param name="response">Page.Response对象</param>
+        /// <param name="fileName">下载文件名</param>
+        /// <param name="fullPath">带文件名下载路径</param>
+        /// <param name="speed">每秒允许下载的字节数</param>
+        /// <returns>返回是否成功</returns>
+        public static bool ResponseFile(HttpRequest request, HttpResponse response, string fileName, string fullPath, long speed = long.MaxValue)
+        {
+            try
+            {
+                FileStream file = new FileStream(fullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                BinaryReader reader = new BinaryReader(file);
+                try
+                {
+                    response.AddHeader("Accept-Ranges", "bytes");
+                    response.Buffer = false;
+                    long fileLength = file.Length;
+                    long startBytes = 0;
+
+                    int pack = 10240; //10K bytes
+                    //int sleep = 200;   //每秒5次   即5*10K bytes每秒
+                    int sleep = (int)Math.Floor((double)(1000 * pack / speed)) + 1;
+                    if (request.Headers["Range"] != null)
+                    {
+                        response.StatusCode = 206;
+                        string[] range = request.Headers["Range"].Split(new char[] { '=', '-' });
+                        startBytes = Convert.ToInt64(range[1]);
+                    }
+                    response.AddHeader("Content-Length", (fileLength - startBytes).ToString());
+                    if (startBytes != 0)
+                    {
+                        response.AddHeader("Content-Range", string.Format(" bytes {0}-{1}/{2}", startBytes, fileLength - 1, fileLength));
+                    }
+                    response.AddHeader("Connection", "Keep-Alive");
+                    response.ContentType = "application/octet-stream";
+                    response.AddHeader("Content-Disposition", "attachment;filename=" + HttpUtility.UrlEncode(fileName, System.Text.Encoding.UTF8));
+
+                    reader.BaseStream.Seek(startBytes, SeekOrigin.Begin);
+                    int maxCount = (int)Math.Floor((double)((fileLength - startBytes) / pack)) + 1;
+
+                    for (int i = 0; i < maxCount; i++)
+                    {
+                        if (response.IsClientConnected)
+                        {
+                            response.BinaryWrite(reader.ReadBytes(pack));
+                            if (speed != long.MaxValue)
+                                Thread.Sleep(sleep);
+                        }
+                        else
+                        {
+                            i = maxCount;
+                        }
+                    }
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    reader.Close();
+                    file.Close();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        #endregion
+
+
+
+        /// <summary>
+        /// 分块下载
+        /// </summary>
+        /// <param name="FileName">文件绝对路径</param>
+        public static void Download(string filePath)
+        {
+            long chunkSize = 204800;             //指定块大小 
+            byte[] buffer = new byte[chunkSize]; //建立一个200K的缓冲区 
+            long dataToRead = 0;                 //已读的字节数   
+            FileStream stream = null;
+            try
+            {
+                //打开文件   
+                stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                dataToRead = stream.Length;
+
+                //添加Http头   
+                HttpContext.Current.Response.ContentType = "application/octet-stream";
+                HttpContext.Current.Response.AddHeader("Content-Disposition", "attachement;filename=" + HttpUtility.UrlEncode(Path.GetFileName(filePath)));
+                HttpContext.Current.Response.AddHeader("Content-Length", dataToRead.ToString());
+
+                while (dataToRead > 0)
+                {
+                    if (HttpContext.Current.Response.IsClientConnected)
+                    {
+                        int length = stream.Read(buffer, 0, Convert.ToInt32(chunkSize));
+                        HttpContext.Current.Response.OutputStream.Write(buffer, 0, length);
+                        HttpContext.Current.Response.Flush();
+                        HttpContext.Current.Response.Clear();
+                        dataToRead -= length;
+                    }
+                    else
+                    {
+                        dataToRead = -1; //防止client失去连接 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpContext.Current.Response.Write("Error:" + ex.Message);
+            }
+            finally
+            {
+                if (stream != null) stream.Close();
+                HttpContext.Current.Response.Close();
+            }
+        }
+    }
+}
